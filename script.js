f2cdc8dab5168dd133ccfa1bc08c5dfaf737f2fa
wanting <wantingpeng.cici@gmail>
2024-08-19 10:57:59 +0200

    callRandomCellFlag
    Do not call randomEmptyCell() when all tiles cannot be merged or moved.

--- script.js
+++ script.js
@@ -52,27 +52,23 @@ function keybord(e){
  }
 
 
- let  randomCell=board.randomEmptyCell()
-  randomCell.tile=new Tile(boardEle)
-
  addListener()
  }
+
  //loop 循环每个column 再循环每个cell 并且拿到里面的cell， 
  //对于每个cell, 比如cell[3] 如果cell[2]为空或同值 则还要继续检查cell[1]， cell[0], 遇到不满足条件的就立刻退出loop,
  //同时记下上一个满足条件的cell 然后移动到最后一个可到达的cell  动了之后要remove原本的tile位置，
 
-
-
  function tileMove(columnArr){
+  let  flag= false
 
   columnArr.forEach(arr => {for(let i=0;i<arr.length ;i++){
-  
     const targetCell=arr[i]; 
     if(targetCell.tileobj==null) continue 
     let lastVaildCell
   for (let j=i-1; j>=0; j--){
     const nextCell=arr[j]
-    if(nextCell.tileobj==null||nextCell.tileobj.value==targetCell.tileobj.value&&!targetCell.tileobj.mergenMark ) lastVaildCell=nextCell
+    if(nextCell.tileobj==null||nextCell.tileobj.value==targetCell.tileobj.value&&!targetCell.tileobj.mergenMark ) {lastVaildCell=nextCell;flag=true}
   else break
   }
 
@@ -91,4 +87,10 @@ continue
   columnArr.forEach(arr=>arr.forEach(cell =>{ if (cell.tileobj)cell.tileobj.mergenMark=false}))
     
  })
+if (flag){
+  let  randomCell=board.randomEmptyCell()
+  randomCell.tile=new Tile(boardEle)
+
+}
+
  } 
